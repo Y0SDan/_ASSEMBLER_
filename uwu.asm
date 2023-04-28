406bfb256316215afcafb9ad80150bd57827471a
Y0SDan <yosd4n@gmail.com>
2023-04-28 09:18:01 -0600

    all of this

--- uwu.asm
+++ uwu.asm
@@ -4,17 +4,18 @@ STACKSG SEGMENT PARA STACK 'STACK'
 STACKSG ENDS
 
 DATASG SEGMENT PARA 'DATA'
-    MEN1       DB '1 -> PARES$'
-    MEN2       DB '2 -> GAUSS$'
-    MEN3       DB '3 -> SUMA ALEATORIO$'
-    MEN4       DB '4 -> SALIR$'
-    MEN5       DB 'Escoge una opcion: $'
-    MEN6       DB 'Ingresa cuantos numeros quieres comprobar$'
-    MEN7       DB 'Escriba un numero$'
-    MEN8       DB 'Pares encontrados$'
-    MEN9       DB 'Total de pares encontrados$'
-    MEN10      DB 'Ingrese el total de numeroS a sumar: $'
-    MEN11      DB 'La suma total es: $'
+    MEN1       DB '                         1 -> PARES$'
+    MEN2       DB '                         2 -> GAUSS$'
+    MEN3       DB '                         3 -> SUMA ALEATORIO$'
+    MEN4       DB '                         4 -> SALIR$'
+    MEN5       DB '                         Escoge una opcion: $'
+    MEN6       DB '  Ingresa cuantos numeros quieres comprobar: $'
+    MEN7       DB '  Escriba un numero: $'
+    MEN8       DB 'Pares encontrados: $'
+    MEN9       DB 'Total de pares encontrados: $'
+    MEN10      DB 'Ingrese el total de numeros a sumar: $'
+    MEN11      DB 'La suma total DE LOS 100 primeros numeros hexadecimales es: $'
+    MEN12      DB 'La suma total es: $'
     NUM1       DB ? 
     CONT       DB 100 DUP(?)
     CONTENIDO  DB 100 DUP("$"),"$"
@@ -32,6 +33,8 @@ CODESG SEGMENT PARA 'CODE'
         MOV DS,AX
 
                     ;MENU
+        regresa:    CALL salta
+                    CALL salta
                     LEA  DX,MEN1 ;1 -> PARES
                     CALL escribeCad
                     CALL salta   ;Alimentar Linea
@@ -44,42 +47,44 @@ CODESG SEGMENT PARA 'CODE'
                     LEA  DX,MEN4 ;4 -> SALIR
                     CALL escribeCad
                     CALL salta
-        regresa:    LEA  DX,MEN5 ;Escoge una opcion
-                    CALL escribeCad
                     CALL salta
+                    LEA  DX,MEN5 ;Escoge una opcion
+                    CALL escribeCad
                     CALL leeChar_conEco
                     CALL salta
-                    CMP  AL,31
+                    CMP  AL,"1"
                     JE   uno    ;pares
                     JL   regresa
-                    CMP  AL,32
+                    CMP  AL,"2"
                     JE   dos   ;gauss
-                    CMP  AL,33
+                    CMP  AL,"3"
                     JE   tres  ;sumAleatorio
-                    CMP  AL,34
+                    CMP  AL,"4"
                     JE   cuatro
                     JG   regresa
         salida:     CALL cuatro ;salir ;salir_dos
 
         uno:        CALL pares
-                    JMP regresa
+                    JMP  regresa
         dos:        CALL gauss            
-                    JMP regresa
+                    JMP  regresa
         tres:       CALL sumAleatorio
-                    JMP regresa
+                    JMP  regresa
         cuatro:     CALL salir            
 
 ;------------------------------------ Subrutinas del programa -------------------------------;
         ;1 -> PARES
         pares PROC
-                    LEA   DX,MEN6 ;Ingresa cuantos numeros quieres comprobar
-                    CALL  escribeCad
-                    CALL  empaqueta
+                    CALL salta          ;Salto de linea
+                    LEA   DX,MEN6       ;Ingresa cuantos numeros quieres comprobar
+                    CALL  escribeCad    ;escribe en consola
+                    CALL  empaqueta     ;convierte de ascci a binario y se guarda en AL
                     CALL  salta
                     SUB   CX,CX
                     MOV   CL,AL
                     MOV   BX,0000
                     LEA   SI,CONT
+                    CALL salta
             NUM:    LEA   DX,MEN7 ;Escriba un numero
                     CALL  escribeCad
                     CALL  empaqueta
@@ -97,7 +102,9 @@ CODESG SEGMENT PARA 'CODE'
                     JMP   L1
             FN:     MOV   CL,BH
                     LEA   DX,MEN8 ;Pares encontrados
+                    CALL  salta
                     CALL  escribeCad
+                    CALL  salta
                     CALL  salta
                     LEA   SI,CONT
             ESV:    MOV   DL,[SI]            
@@ -105,6 +112,7 @@ CODESG SEGMENT PARA 'CODE'
                     CALL  salta
                     INC   SI
                     LOOP  ESV
+                    CALL salta
                     LEA   DX,MEN9 ;Total de pares encontrados
                     CALL  escribeCad
                     MOV   DL,BH
@@ -115,6 +123,9 @@ CODESG SEGMENT PARA 'CODE'
 
         ;2 -> GAUSS
         gauss PROC
+                CALL salta
+                LEA DX,MEN11
+                CALL escribeCad
                 MOV  CX,64H
                 SUB  BX,BX
             L:  ADD  BX,CX
@@ -128,6 +139,7 @@ CODESG SEGMENT PARA 'CODE'
 
         ;3 -> aLEATORIO
         sumAleatorio PROC
+                CALL salta
                 LEA  DX,MEN10
                 CALL escribeCad
                 CALL empaqueta
@@ -144,7 +156,7 @@ CODESG SEGMENT PARA 'CODE'
                 CALL leeChar_conEco
                 POP  CX
                 LOOP NA
-                LEA  DX,MEN11
+                LEA  DX,MEN12
                 CALL escribeCad
                 MOV  DL,BH
                 CALL desempaqueta
@@ -219,7 +231,7 @@ CODESG SEGMENT PARA 'CODE'
             RET
         escribeCad ENDP
 
-        ;salto de linea y retorno de carro
+        ;salto de linea y retorno de carrok
         salta PROC
             PUSH DX
             MOV  DL,0AH
